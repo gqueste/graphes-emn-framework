2e71441bb5023963482451691888af6f1fbcd42f
gqueste <gabriel.queste@gmail.com>
2015-02-10 14:46:27 +0100

    Ajout Matrice avec valeurs

---
+++ src/graph/ValuatedAdjacencyMatrixUndirectedGraph.java
@@ -0,0 +1,104 @@
+package graph;
+
+public class ValuatedAdjacencyMatrixUndirectedGraph implements IUndirectedGraph {
+private int[][] adjacencyMatrix;
+	
+	public ValuatedAdjacencyMatrixUndirectedGraph(int[][] adjacencyMatrix) {
+		this.adjacencyMatrix = adjacencyMatrix;
+	}
+	public ValuatedAdjacencyMatrixUndirectedGraph(int n){
+		int[][] adjacencyMatrix = new int[n][n];
+		for (int i = 0 ; i< n ; i++){
+			for (int j = 0 ; j < n ; j++){
+				if(i!=j){
+					adjacencyMatrix[i][j] = Integer.MAX_VALUE;
+				}
+				else{
+					adjacencyMatrix[i][i] = 0;
+				}
+			}
+		}
+		this.adjacencyMatrix = adjacencyMatrix;
+		
+	}
+	
+	public int get(int x , int y ){
+		return this.adjacencyMatrix[x][y];
+	}
+	public void set(int x, int y , int value){
+		this.adjacencyMatrix[x][y] = value;
+	}
+	
+
+	public void setAdjacencyMatrix(int[][] adjacencyMatrix) {
+		this.adjacencyMatrix = adjacencyMatrix;
+	}
+
+	@Override
+	public int getNbNodes() {
+		return this.toAdjacencyMatrix().length;
+	}
+
+	@Override
+	public int[][] toAdjacencyMatrix() {
+		return this.adjacencyMatrix;
+	}
+
+	@Override
+	public int getNbEdges() {
+		int res = 0 ;
+		for (int i=0; i<this.adjacencyMatrix.length;i++){
+			for (int j = i; j<this.adjacencyMatrix[i].length ; j++){
+				if(this.adjacencyMatrix[i][j] >=1){
+					res++;
+				}
+			}
+		}
+		return res;
+	}
+
+	@Override
+	public boolean isEdge(int x, int y) {
+		return this.get(x, y)>=1 && this.get(x, y)<Integer.MAX_VALUE;
+	}
+
+	@Override
+	public void removeEdge(int x, int y) {
+		this.set(x, y, Integer.MAX_VALUE);
+		this.set(y, x, Integer.MAX_VALUE);
+	}
+
+	
+	public void addEdge(int x, int y, int value) {
+		this.set(x, y, value);
+		this.set(y, x, value);
+	}
+	
+	@Override
+	public void addEdge(int x, int y) {
+		this.set(x, y, 1);
+		this.set(y, x, 1);
+	}
+	
+	
+
+	@Override
+	public int[] getNeighbors(int x) {
+		int compteur = 0 ; 
+		for (int i = 0 ; i<this.adjacencyMatrix[x].length ; i++){
+			if (x!=i && this.get(x, i)<Integer.MAX_VALUE){
+				compteur++;
+			}
+		}
+
+		int[] voisins = new int[compteur];
+		int offset = 0;
+		for (int j = 0 ; j< this.adjacencyMatrix[x].length ; j++){
+			if( x!=j && this.get(x, j)<Integer.MAX_VALUE){
+				voisins[offset] = j;
+				offset++;
+			}
+		}
+		return voisins;
+	}
+}
